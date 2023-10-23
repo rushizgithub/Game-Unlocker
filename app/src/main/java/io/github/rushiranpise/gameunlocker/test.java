74fa5d15f66ee81d1f509381ad59a7fa3a3ac0ca
Rushi Ranpise <136841749+rushizgithub@users.noreply.github.com>
2023-10-24 02:10:28 +0530

    6

--- app/src/main/java/io/github/rushiranpise/gameunlocker/test.java
+++ app/src/main/java/io/github/rushiranpise/gameunlocker/test.java
@@ -23,20 +23,6 @@ public class test implements IXposedHookLoadPackage {
     @Override
 public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
 
-    // Hook the Settings.Secure.getInt() method.
-    XposedHelpers.findAndHookMethod("android.provider.Settings$Secure", lpparam.classLoader, "getInt", String.class, int.class, new XC_MethodHook() {
-
-        @Override
-        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-
-            // Check if the setting name is DEVELOPMENT_SETTINGS_ENABLED.
-            if (param.args[0].equals(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED)) {
-
-                // Replace the return value with 1, regardless of the actual state of the setting.
-                param.setResult(1);
-            }
-        }
-    });
 
     // Hook the getValueFromProp() method.
     XposedHelpers.findAndHookMethod("com.byxiaorun.detector", lpparam.classLoader, "getValueFromProp", String.class, new XC_MethodHook() {
