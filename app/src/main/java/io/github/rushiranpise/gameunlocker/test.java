c32791a40951acbf793d5d590f9c39e0e478be10
Rushi Ranpise <136841749+rushizgithub@users.noreply.github.com>
2023-10-24 02:13:25 +0530

    66

--- app/src/main/java/io/github/rushiranpise/gameunlocker/test.java
+++ app/src/main/java/io/github/rushiranpise/gameunlocker/test.java
@@ -1,42 +1,35 @@
 package io.github.rushiranpise.test;
 
-import android.annotation.SuppressLint;
-import android.os.Build;
-import android.util.Log;
-
-import java.lang.reflect.Field;
-import java.security.KeyStore;
-import java.util.Arrays;
-
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-@SuppressLint("DiscouragedPrivateApi")
-@SuppressWarnings("ConstantConditions")
-public class test implements IXposedHookLoadPackage {
+import java.util.HashSet;
+import java.util.Set;
+import android.util.Log;
 
+public class PropertyHook implements IXposedHookLoadPackage {
+
+    private static final String PROP_USB_FFS_READY = "sys.usb.ffs.ready";
+    private static final String PROP_USB_STATE = "sys.usb.state";
+    private static final String PROP_USB_CONFIG = "sys.usb.config";
+    private static final String PROP_USB_REBOOT_FUNNC = "persist.sys.usb.reboot.funnc";
 
     @Override
-public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedHelpers.findAndHookMethod("com.byxiaorun.detector", lpparam.classLoader, "getValueFromProp", String.class, new XC_MethodHook() {
 
+            private static final Set<String> PROPERTIES_TO_HOOK = new HashSet<>(Arrays.asList(
+                    PROP_USB_FFS_READY, PROP_USB_STATE, PROP_USB_CONFIG, PROP_USB_REBOOT_FUNNC));
 
-    // Hook the getValueFromProp() method.
-    XposedHelpers.findAndHookMethod("com.byxiaorun.detector", lpparam.classLoader, "getValueFromProp", String.class, new XC_MethodHook() {
-
-        @Override
-        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-
-            // Check if the property name is sys.usb.ffs.ready, sys.usb.state, sys.usb.config, or persist.sys.usb.reboot.funnc.
-            if (param.args[0].equals("sys.usb.ffs.ready") || param.args[0].equals("sys.usb.state") || param.args[0].equals("sys.usb.config") || param.args[0].equals("persist.sys.usb.reboot.funnc")) {
-
-                // Replace the return value with a string that contains the value "1".
-                param.setResult("1");
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                String propertyName = (String) param.args[0];
+                if (PROPERTIES_TO_HOOK.contains(propertyName)) {
+                    param.setResult("1");
+                }
             }
-        }
-    });
-}
+        });
+    }
 }
